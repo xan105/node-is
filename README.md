9f6e8f7191ab4d765ef5149e7032765e07319767
Anthony Beaumont <dev.xan105@outlook.be>
2021-10-20 19:00:14 +0700

    sync

--- README.md
+++ README.md
@@ -1,38 +1,48 @@
-Check if a binary is 32 (x86) or 64-bit (x86_64).<br />
+About
+=====
+
+Collection of various 'is ... ?' checks.
+
+Install
+=======
+
+`npm install @xan105/is`
+
+API
+===
+
+⚠️ This module is only available as an ECMAScript module (ESM)<br />
+
+binary is 32 (x86) or
+
+## Named export
+
+### bin
+
+#### `is64bit (filePath: string, platform?: string): bool`
+
+Check if a 64-bit (x86_64) binary.<br />
 
 📖 cf:
 - https://docs.microsoft.com/en-us/windows/win32/debug/pe-format
 - https://docs.microsoft.com/en-us/windows/win32/debug/pe-format#machine-types
 - https://en.wikipedia.org/wiki/Executable_and_Linkable_Format#File_header
 
-Example
-=======
+##### Example
+
 ```js 
-const is64bit = require('is64bit');
-
-is64bit("hello_world.exe").then((is64) => { 
-  console.log(`64-bit: ${is64}`) //True or false
-}).catch((err) => { console.error(err) });
-
-//async/await
-(async ()=>{
-  try{
-    let is64 = await is64bit("hello_world.exe");
-  }catch(err){
-    console.error(err);
-  }
-})();
-
+import { is64bit } from "@xan105/is/bin');
+const is64 = await is64bit("hello_world.exe"); //True or false
 ```
 
 Check binary from another platform
-=====================================
-```js 
-//Linux binary with Node running on Windows
-const is64bit = require('is64bit');
 
-is64bit("hello_world","linux") //same values as os.platform()
-.then((is64) => { 
-  console.log(`64-bit: ${is64}`) //True or false
-}).catch((err) => { console.error(err) });
+```js 
+import { is64bit } from "@xan105/is/bin');
+//Linux binary with Node running on Windows
+const is64 = await is64bit("hello_world","linux") //same values as os.platform()
 ```
+
+#### `is32bit (filePath: string, platform?: string): bool`
+
+Same as above but for a 32-bit (x86) binary.
---
+++ test/file.js
@@ -0,0 +1,42 @@
+import t from 'tap';
+import { readFileSync as readFile } from "node:fs";
+import { fileURLToPath } from "node:url";
+import { join, dirname } from "node:path";
+const __dirname = dirname(fileURLToPath(import.meta.url));
+import { is64bit, is32bit } from "../lib/is/file/binary.js";
+import { isPNG, isJPG, isICO } from "../lib/is/file/image.js";
+
+t.test('binary', async t => { 
+
+t.ok(await is64bit(join(__dirname,"./sample/wget64"), "linux"), "is a linux x64 binary");
+t.notOk(await is64bit(join(__dirname,"./sample/wget86"), "linux"), "is not a linux x64 binary");
+t.notOk(await is32bit(join(__dirname,"./sample/wget64"), "linux"), "is not a linux x86 binary");
+t.ok(await is32bit(join(__dirname,"./sample/wget86"), "linux"), "is a linux x86 binary");
+
+t.ok(await is64bit(join(__dirname,"./sample/hello64.exe"), "win32"), "is a windows x64 binary");
+t.notOk(await is64bit(join(__dirname,"./sample/hello86.exe"), "win32"), "is not a windows x64 binary");
+t.notOk(await is32bit(join(__dirname,"./sample/hello64.exe"), "win32"), "is not a windows x86 binary");
+t.ok(await is32bit(join(__dirname,"./sample/hello86.exe"), "win32"), "is a windows x86 binary");
+
+t.rejects(is64bit(join(__dirname,"./sample/blank.txt")), "not a binary");
+t.rejects(is64bit(join(__dirname,"./sample/truelove")),"file doesn't exist");
+
+t.end();
+});
+
+t.test('image', t => {
+
+t.ok(isPNG(readFile(join(__dirname,"./sample/winner.png"))), "is a valid png image file");
+t.ok(isJPG(readFile(join(__dirname,"./sample/winner.jpg"))), "is a valid jpg image file");
+t.ok(isICO(readFile(join(__dirname,"./sample/winner.ico"))), "is a valid ico image file");
+
+t.notOk(isPNG(readFile(join(__dirname,"./sample/winner.jpg"))), "is not a valid png image file");
+t.notOk(isJPG(readFile(join(__dirname,"./sample/winner.png"))), "is not a valid jpg image file");
+t.notOk(isICO(readFile(join(__dirname,"./sample/winner.png"))), "is not a valid ico image file");
+
+t.throws(function(){ isPNG(readFile(join(__dirname,"./sample/blank.txt"),"utf8")) }, {code: "ERR_NOT_A_BUFFER"}, "not a buffer");
+t.throws(function(){ isJPG(readFile(join(__dirname,"./sample/blank.txt"),"utf8")) }, {code: "ERR_NOT_A_BUFFER"}, "not a buffer");
+t.throws(function(){ isICO(readFile(join(__dirname,"./sample/blank.txt"),"utf8")) }, {code: "ERR_NOT_A_BUFFER"}, "not a buffer");
+
+t.end();
+});
