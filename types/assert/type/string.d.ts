0f723cac2af2effcf9ea39a6b2061d2880c8de95
Anthony Beaumont <dev.xan105@outlook.be>
2024-05-10 22:34:47 +0700

    wip

--- types/assert/type/string.d.ts
+++ types/assert/type/string.d.ts
@@ -1,4 +1,4 @@
 export function shouldString(value: unknown, error?: Error | string | null): void;
 export function shouldStringNotEmpty(value: unknown, error?: Error | string | null): void;
-export function shouldStringLike(value: unknown, pattern: RegExp, error?: Error | string | null): void;
+export function shouldStringLike(value: unknown, pattern: RegExp | string, error?: Error | string | null): void;
 export function shouldHexString(value: unknown, error?: Error | string | null): void;
